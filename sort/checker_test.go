e3e427af3d84b3bad4cf5a109d24347c7de8a21b
stone <335612970@qq.com>
2020-06-05 15:04:31 +0800

    排序：1.计数排序（不稳定） 2.累加计数排序（稳定）

--- sort/checker_test.go
+++ sort/checker_test.go
@@ -3,14 +3,14 @@ package sort
 import "testing"
 
 func TestCheckSort(t *testing.T) {
-	//CheckSort(Selection, 1000000, 750
-	//CheckSort(Bubble, 1000000, 750)
-	//CheckSort(Insertion, 1000000, 750)
-	//CheckSort(Shell, 1000000, 750)
-	//CheckSort(Shell2, 1000000, 750)
-	//CheckSort(Merge, 1000000, 750)
-	//CheckSort(SinglePivotQuick, 1000000, 750)
-	//CheckSort(DualPivotQuick,1000000, 750)
-	//CheckSort(Counting, 1000000, 750)
-	//CheckSort(SumCounting, 1000000, 750)
+	CheckSort(Selection, 1000000, 750)
+	CheckSort(Bubble, 1000000, 750)
+	CheckSort(Insertion, 1000000, 750)
+	CheckSort(Shell, 1000000, 750)
+	CheckSort(Shell2, 1000000, 750)
+	CheckSort(Merge, 1000000, 750)
+	CheckSort(SinglePivotQuick, 1000000, 750)
+	CheckSort(DualPivotQuick, 1000000, 750)
+	CheckSort(Counting, 1000000, 750)
+	CheckSort(SumCounting, 1000000, 750)
 }
